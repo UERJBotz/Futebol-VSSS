4b5e8c4525b370d091f9140d79b8524f5c455b2c
theo-al <emailtheoprauerj@gmail.com>
2025-02-14 18:00:12 -0300

    melhoria: alerta de pouca bateria

--- firmware/player/player.ino
+++ firmware/player/player.ino
@@ -61,12 +61,24 @@ void setup() {
     Serial.printf("MAC: %02x:%02x:%02x:%02x:%02x:%02x\n",
                   mac_addr[0], mac_addr[1], mac_addr[2],
                   mac_addr[3], mac_addr[4], mac_addr[5]);
-    Serial.printf("ID: %d", ID);
+    Serial.printf("ID: %d\n", ID);
 
     esp_now_register_recv_cb(esp_now_recv_cb_t(on_recv));
+
+    Serial.printf("Bateria em %dmV", batt());
+    bipe(200); delay(200); bipe(200);
 }
 
 void loop() {
+    auto bat = batt(); //mV
+    if (bat < 7000) {
+        Serial.printf("[ALERTA]: Pouca bateria: %dmV\n", bat);
+
+    	bipe(200); delay(200);
+    	bipe(200); delay(200);
+    	bipe(400); delay(200);
+    }
+
     static struct vel prev{0}, vel{0};
 
     vel = vels.of[ID];
--- firmware/robot.h
+++ firmware/robot.h
@@ -1,5 +1,6 @@
-void     move(uint8_t, uint8_t);
-uint32_t batt(void);
+void     bipe(int); //millis
+void     move(uint8_t, uint8_t); //%, %
+uint32_t batt(void); //mV
 
 #ifdef VESPA
     #include <RoboCore_Vespa.h>
@@ -13,7 +14,12 @@ uint32_t batt(void);
         motors.turn(esq, dir);
     }
     uint32_t batt() {
-        return vbat.readVoltage();
+        return vbat.readVoltage(); //mV
+    }
+    void bipe(int dt) {
+	move(20, 20);
+	delay(dt);
+	move(0, 0);
     }
 #else
     static_assert(0, "robôs por enquanto só com a vespa");
