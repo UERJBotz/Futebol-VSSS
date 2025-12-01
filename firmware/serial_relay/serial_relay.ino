6716ea20b0bad4a671c5d09e874a2cd7c3759f15
theo-al <emailtheoprauerj@gmail.com>
2025-12-01 20:41:07 -0300

    firmware: serial_relay: agora com telinha
    usei uma tela oled 0.91 inch que tava jogada por aqui, agora o
    transmissor depende da biblioteca u8g2 (para a comunicação com ela)
    
    eu vou tentar deixar ela opcional depois

--- firmware/serial_relay/serial_relay.ino
+++ firmware/serial_relay/serial_relay.ino
@@ -1,12 +1,18 @@
 #include <Arduino.h>
+#include <U8g2lib.h>
+#include <SPI.h>  //! checar se precisa mesmo
+#include <Wire.h> //! checar se precisa mesmo
 #include "../comms.h"
 
 #define BAUD_RATE 115200
-
+#define FONTE u8g2_font_missingplanet_tf
+//choose a suitable font at https://github.com/olikraus/u8g2/wiki/fntlistall
 
 uint8_t peer_addr[6] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
 
-char input[255] = {0};
+U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C oled(U8G2_R2);
+
+
 void setup() {
     init_wifi();
     uint8_t* mac_addr = get_mac_addr();
@@ -21,14 +27,24 @@ void setup() {
         .encrypt = false,
     };
     memcpy(peer.peer_addr, peer_addr, sizeof(peer_addr));
-    
+
     esp_err_t err = esp_now_add_peer(&peer);
     assert (err == ESP_OK);
+
+    oled.begin();
+    oled.setFont(FONTE);
+
+    oled.clearBuffer();            // clear the internal memory
+    oled.drawStr(10,29, "ligado"); // write something to the internal memory
+    oled.sendBuffer();             // transfer internal memory to the display
 }
 
+unsigned long last_pkg_sent = 0;
 void loop() {
-    const char sentinel = '\n';
+    static char input[256];
+    static const char sentinel = '\n';
 
+    if (millis() - last_pkg_sent < 50) return;
     for (auto buf = input; Serial.available(); buf++) {
         *buf = (char) Serial.read();
         if (*buf == sentinel) {
@@ -43,10 +59,16 @@ void loop() {
             esp_err_t err = esp_now_send(peer_addr, (uint8_t*) &msg, sizeof(msg));
             if (err == ESP_OK) Serial.println("Sent with success");
             else               Serial.println("Error sending the data");
+            last_pkg_sent = millis();
         }
     }
     // esse loop lê da serial até encontrar um \n e aí faz broadcast via espnow
     // se a mensagem parar de chegar antes disso, ele ignora
     // detalhe: Serial.available() retorna quantos bytes tão disponíveis
-}
 
+    if (last_pkg_sent) {
+        oled.clearBuffer();
+        oled.drawStr(1,10, input);
+        oled.sendBuffer();
+    }
+}
